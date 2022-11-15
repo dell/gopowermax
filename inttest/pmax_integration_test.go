5e0bfe1842f232f0579f96dfc0c51f1dc99bc406
SoorajBharadwaj <sooraj_bharadwaj@dell.com>
2022-11-15 11:43:40 +0530

    Updating testcase

--- inttest/pmax_integration_test.go
+++ inttest/pmax_integration_test.go
@@ -1655,6 +1655,7 @@ func TestUpdatePortGroup(t *testing.T) {
 	portKeys = portGroup.SymmetrixPortKey
 	if portKeys[0].DirectorID != "OR-2C" {
 		t.Errorf("Couldnt modify port group details: %s", err.Error())
+		return
 	}
 
 	err = client.DeletePortGroup(context.TODO(), symmetrixID, newPortGroupID)
