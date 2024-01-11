4c39b068cee81f98dc2f3f16ba3af02def7ed901
Utkarsh Dubey <Utkarsh.Dubey@Dell.com>
2024-01-11 19:08:41 +0530

    Adds file System metrics

--- metrics.go
+++ metrics.go
@@ -29,6 +29,7 @@ const (
 	Performance  = "performance"
 	StorageGroup = "/StorageGroup"
 	Volume       = "/Volume"
+	FileSystem   = "/file/filesystem"
 	Metrics      = "/metrics"
 	Keys         = "/keys"
 	Array        = "/Array"
@@ -139,6 +140,73 @@ func (c *Client) GetVolumesMetrics(ctx context.Context, symID string, storageGro
 		return nil, err
 	}
 	metricsList := &types.VolumeMetricsIterator{}
+	decoder := json.NewDecoder(resp.Body)
+	if err = decoder.Decode(metricsList); err != nil {
+		return nil, err
+	}
+	err = resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	return metricsList, nil
+}
+
+// GetVolumesMetricsByID returns a given Volume performance metrics
+func (c *Client) GetVolumesMetricsByID(ctx context.Context, symID string, volID string, metricsQuery []string, firstAvailableTime, lastAvailableTime int64) (*types.VolumeMetricsIterator, error) {
+	defer c.TimeSpent("GetVolumesMetricsByID", time.Now())
+	if _, err := c.IsAllowedArray(symID); err != nil {
+		return nil, err
+	}
+	URL := RESTPrefix + Performance + Volume + Metrics
+	ctx, cancel := c.GetTimeoutContext(ctx)
+	defer cancel()
+	params := types.VolumeMetricsParam{
+		SystemID:         symID,
+		StartDate:        firstAvailableTime,
+		EndDate:          lastAvailableTime,
+		VolumeStartRange: volID,
+		VolumeEndRange:   volID,
+		DataFormat:       Average,
+		Metrics:          metricsQuery,
+	}
+	resp, err := c.api.DoAndGetResponseBody(ctx, http.MethodPost, URL, c.getDefaultHeaders(), params)
+	if err = c.checkResponse(resp); err != nil {
+		return nil, err
+	}
+	metricsList := &types.VolumeMetricsIterator{}
+	decoder := json.NewDecoder(resp.Body)
+	if err = decoder.Decode(metricsList); err != nil {
+		return nil, err
+	}
+	err = resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	return metricsList, nil
+}
+
+// GetFileSystemMetricsByID returns a given FileSystem performance metrics
+func (c *Client) GetFileSystemMetricsByID(ctx context.Context, symID string, fsID string, metricsQuery []string, firstAvailableTime, lastAvailableTime int64) (*types.FileSystemMetricsIterator, error) {
+	defer c.TimeSpent("GetFileSystemMetricsByID", time.Now())
+	if _, err := c.IsAllowedArray(symID); err != nil {
+		return nil, err
+	}
+	URL := RESTPrefix + Performance + FileSystem + Metrics
+	ctx, cancel := c.GetTimeoutContext(ctx)
+	defer cancel()
+	params := types.FileSystemMetricsParam{
+		SystemID:     symID,
+		StartDate:    firstAvailableTime,
+		EndDate:      lastAvailableTime,
+		DataFormat:   Average,
+		FileSystemID: fsID,
+		Metrics:      metricsQuery,
+	}
+	resp, err := c.api.DoAndGetResponseBody(ctx, http.MethodPost, URL, c.getDefaultHeaders(), params)
+	if err = c.checkResponse(resp); err != nil {
+		return nil, err
+	}
+	metricsList := &types.FileSystemMetricsIterator{}
 	decoder := json.NewDecoder(resp.Body)
 	if err = decoder.Decode(metricsList); err != nil {
 		return nil, err
