f1e2f4e1655b4b251b0840667a38fd7da3f9f41d
Surya <surya.gupta1@dell.com>
2025-01-08 05:05:40 +0000

    remove unused variable

--- api/api_test.go
+++ api/api_test.go
@@ -534,7 +534,7 @@ func TestDoLog(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(tt.name, func(_ *testing.T) {
 			c := &client{
 				debug: tt.fields.debug,
 			}
