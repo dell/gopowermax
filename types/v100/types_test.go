a73dd4a2a541b84e24da8fc34c3530ae31703e35
Harshita Pandey <Harshita_Pandey@dell.com>
2025-02-12 23:07:23 +0530

    Added Unit tests for types.go file

---
+++ types/v100/types_test.go
@@ -0,0 +1,87 @@
+package v100
+
+import "testing"
+
+func TestGetJobResource(t *testing.T) {
+	tests := []struct {
+		name                 string
+		job                  Job
+		expectedSymmetrixID  string
+		expectedResourceType string
+		expectedResourceID   string
+	}{
+		{
+			name: "valid resource link",
+			job: Job{
+				ResourceLink: "provisioning/system/SYMMETRIX-1234/volume/1234",
+			},
+			expectedSymmetrixID:  "SYMMETRIX-1234",
+			expectedResourceType: "volume",
+			expectedResourceID:   "1234",
+		},
+		{
+			name: "Empty resource link",
+			job: Job{
+				ResourceLink: "",
+			},
+			expectedSymmetrixID:  "",
+			expectedResourceType: "",
+			expectedResourceID:   "",
+		},
+		{
+			name: "invalid resource link",
+			job: Job{
+				ResourceLink: "system/SYMMETRIX-1234",
+			},
+			expectedSymmetrixID:  "",
+			expectedResourceType: "",
+			expectedResourceID:   "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			symID, resourceType, id := tt.job.GetJobResource()
+
+			if symID != tt.expectedSymmetrixID {
+				t.Errorf("expected %s, got %s", tt.expectedSymmetrixID, symID)
+			}
+
+			if resourceType != tt.expectedResourceType {
+				t.Errorf("expected %s, got %s", tt.expectedResourceType, resourceType)
+			}
+
+			if id != tt.expectedResourceID {
+				t.Errorf("expected %s, got %s", tt.expectedResourceID, id)
+			}
+		})
+	}
+}
+
+func TestError(t *testing.T) {
+	tests := []struct{
+		name string
+		err *Error
+	} {
+		{
+			name: "valid error",
+			err: &Error{
+				Message: "test-error",
+			},
+		},
+		{
+			name: "empty error",
+			err: &Error{
+				Message: "",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.err.Error() != tt.err.Message {
+				t.Errorf("expected %s, got %s", tt.err.Message, tt.err.Error())
+			}
+		})
+	}
+}
